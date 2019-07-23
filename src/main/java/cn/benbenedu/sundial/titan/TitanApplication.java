030a72e7c01e5b44aeadc2bab99c794dac892578
Zhaopeng Gong <zhaopeng@ZhaopengdeiMac.local>
2019-07-23 10:33:22 +0800

    Initializing...

---
+++ src/main/java/cn/benbenedu/sundial/titan/TitanApplication.java
@@ -0,0 +1,13 @@
+package cn.benbenedu.sundial.titan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TitanApplication.class, args);
+    }
+
+}
